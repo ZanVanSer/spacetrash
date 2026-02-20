2ebe1b46982bbe4b91fed7e11b6feefcf2cb734f
zan <17440488+ZanVanSer@users.noreply.github.com>
2026-02-20 12:49:42 +0200

    dataloader test

--- main.lua
+++ main.lua
@@ -1,6 +1,10 @@
 local sm = require "states/statemanager"
+local dl = require "systems/dataloader"
 
 function love.load()
+    local weapons = dl.getWeapons()
+    local ships = dl.getShips()
+
     sm.register("test1", require "states/test1")
     sm.register("test2", require "states/test2")
     sm.switch("test1")
