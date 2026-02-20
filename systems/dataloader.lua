1a2355c15516b4e6afbbc6d09051c0ff16df6282
zan <17440488+ZanVanSer@users.noreply.github.com>
2026-02-20 12:42:42 +0200

    dataloader system

---
+++ systems/dataloader.lua
@@ -0,0 +1,37 @@
+local json = require "systems/json"
+local dl = { cache = {} }
+
+function dl.loadJSON(filename)
+    if dl.cache[filename] then return dl.cache[filename] end
+    local path = "data/" .. filename .. ".json"
+    local info = love.filesystem.getInfo(path)
+    if not info then return {} end
+    
+    local content, size = love.filesystem.read(path)
+    if not content then return {} end
+    
+    local data = json.decode(content)
+    dl.cache[filename] = data
+    return data
+end
+
+function dl.createLookup(dataArray, keyField)
+    local lookup = {}
+    for _, item in ipairs(dataArray) do
+        if item[keyField] then
+            lookup[item[keyField]] = item
+        end
+    end
+    return lookup
+end
+
+local function createGetter(name)
+    dl["get" .. name:gsub("^%l", string.upper)] = function()
+        return dl.loadJSON(name)
+    end
+end
+
+local types = {"weapons", "ships", "enemies", "bosses", "stages", "upgrades"}
+for _, t in ipairs(types) do createGetter(t) end
+
+return dl
